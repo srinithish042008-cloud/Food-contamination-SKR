1619e630fc11b134ad9418e0360ee5de89f07d15
srinithish042008-cloud <srinithish042008@gmail.com>
2026-04-03 23:35:20 +0530

    Create Aim-theory .py

---
+++ Aim-theory .py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("📘 Aim & Theory")
+
+st.subheader("🎯 Aim")
+st.write("To study the effect of storage and hygiene on food contamination.")
+
+st.subheader("📖 Theory")
+st.write("""
+Microbial growth depends on temperature, hygiene, and food type.
+Cold storage slows growth, while poor hygiene increases contamination.
+""")
